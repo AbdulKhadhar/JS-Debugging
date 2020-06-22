56e7114548a94b768c018b41a380b4707c571b04
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-22 22:26:46 +0530

    Catch Misspelled Variable and Function Names

---
+++ MisspelledVar.js
@@ -0,0 +1,5 @@
+let receivables = 10;
+let payables = 8;
+// Spelled wrong : let netWorkingCapital = recievables - payable;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
