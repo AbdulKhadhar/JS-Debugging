b29c2c339786909e9929dc173d09673868b0c61c
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-22 22:21:38 +0530

    Use typeof to Check the Type of a Variable

---
+++ typeof.js
@@ -0,0 +1,10 @@
+console.log(typeof ""); // outputs "string"
+console.log(typeof 0); // outputs "number"
+console.log(typeof []); // outputs "object"
+console.log(typeof {}); // outputs "object"
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof seven);
+console.log(typeof three)
