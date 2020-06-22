3267853d43f4680229ef5b1fb4482e8633c33f87
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-22 22:35:17 +0530

    Catch Mixed Usage of Single and Double Quotes

---
+++ mixedQoutes.js
@@ -0,0 +1,13 @@
+// These are correct:
+const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
+const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+// This is incorrect:
+const uhOhGroucho = 'I've had a perfectly wonderful evening, but this wasn't it.';
+
+
+// Correct use of same quotes:
+const allSameQuotes = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
+
+
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
