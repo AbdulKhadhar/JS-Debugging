2b754645364547a103574b6dfc7bea597b874e89
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-22 22:18:55 +0530

    Clear

---
+++ consoleclear.js
@@ -0,0 +1,9 @@
+// Open your browser console.
+let output = "twice in the browser console";
+
+// Use console.log() to print the output variable.
+
+console.log(output)
+
+// Clear console
+console.clear()
