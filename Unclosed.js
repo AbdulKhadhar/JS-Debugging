abf494f2782af2d7ade854469d98c86d22932e69
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-06-22 22:30:29 +0530

    Unclosed Parentheses, Brackets, Braces and Quotes
    Catch Unclosed Parentheses, Brackets, Braces and Quotes

---
+++ Unclosed.js
@@ -0,0 +1,5 @@
+// let myArray = [1, 2, 3; bracket is not closed
+let myArray = [1, 2, 3];
+// let arraySum = myArray.reduce((previous, current =>  previous + current); one bracket is not closed
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
